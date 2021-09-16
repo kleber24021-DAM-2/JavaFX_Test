915ed1092f775b99e2ab66eb6870c75b260db5aa
kleber24021 <kleber24021@gmail.com>
2021-09-16 23:44:49 +0200

    Initial commit de el proyecto de JavaFX personal. De momento son dos listas en las que se pueden añadir elementos, eliminarlos y moverlo entre listas.

---
+++ src/main/java/main/MainFX.java
@@ -0,0 +1,26 @@
+package main;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MainFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loaderMenu = new FXMLLoader(getClass().getResource("/fxml/Prueba.fxml"));
+        AnchorPane root = loaderMenu.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Mi primera ventana de prueba");
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+    }
+}
